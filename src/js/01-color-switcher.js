fe38fda472c64c6dc1763b997e1641333a8e71a6
Inna Pokydko <inna.pokydko@gmail.com>
2023-02-20 20:14:18 +0200

    tas1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const refs = {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+let disabled = true;
+let timerId = null;
+
+refs.startBtn.addEventListener('click', onStart);
+refs.stopBtn.addEventListener('click', onStop);
+
+
+
+function onStart() {
+  timerId = setInterval(bgColor, 1000);
+  
+  refs.startBtn.setAttribute('disabled', '');
+  }
+
+function onStop() {
+  clearInterval(timerId);
+
+  refs.startBtn.removeAttribute('disabled');
+}
+
+function bgColor() {
+  refs.body.style.backgroundColor = getRandomHexColor();
+}
---
+++ src/js/02-timer.js
@@ -0,0 +1,28 @@
+
+const timer = setInterval(function() {
+  // Отримуємо поточну дату та час
+  const now = new Date().getTime();
+
+  // Знаходимо різницю між поточною датою та часом та датою та часом до якої відлікати таймер
+  const distance = countDownDate - now;
+
+  // Розраховуємо дні, години, хвилини та секунди
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Виводимо результат у відповідне місце на сторінці
+  const timerElement = document.getElementById("timer");
+  timerElement.innerHTML = "До кінця року залишилось: " + days + "д " + hours + "г " + minutes + "хв " + seconds + "с ";
+
+  // Якщо таймер досягнув цілі, зупиняємо його та виводимо повідомлення
+  if (distance < 0) {
+    clearInterval(timer);
+    timerElement.innerHTML = "Час вийшов!";
+  }
+}, 1000);
+
+
+
+
