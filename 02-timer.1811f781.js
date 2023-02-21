126fcc777ad2d6a8344f4b4f7b1b4f59309f8c57
InnaPokydko <112860077+InnaPokydko@users.noreply.github.com>
2023-02-21 17:56:14 +0000

    Deploying to gh-pages from @ InnaPokydko/goit-js-hw-09@8741d2a4309ad209f615ef31b7c0a8039351382a 🚀

--- 02-timer.1811f781.js
+++
@@ -1,2 +0,0 @@
-const e=setInterval((function(){const t=(new Date).getTime(),n=countDownDate-t,o=Math.floor(n/864e5),r=Math.floor(n%864e5/36e5),a=Math.floor(n%36e5/6e4),l=Math.floor(n%6e4/1e3),c=document.getElementById("timer");c.innerHTML="До кінця року залишилось: "+o+"д "+r+"г "+a+"хв "+l+"с ",n<0&&(clearInterval(e),c.innerHTML="Час вийшов!")}),1e3);
-//# sourceMappingURL=02-timer.1811f781.js.map
