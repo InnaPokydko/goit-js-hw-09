72a899de0f82dc9b3300a4542cd937d2814b8c79
Inna Pokydko <inna.pokydko@gmail.com>
2023-02-22 14:14:22 +0200

    task2?

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,6 +1,6 @@
-import flatpickr from "flatpickr";
+import flatpickr from 'flatpickr';
 
-import "flatpickr/dist/flatpickr.min.css";
+import 'flatpickr/dist/flatpickr.min.css';
 
 const refs = {
   text: document.querySelector('#datetime-picker'),
@@ -10,15 +10,20 @@ const refs = {
   dataMin: document.querySelector('value[data-minutes]'),
   dataSec: document.querySelector('value[data-seconds]'),
 };
+
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
-  
+
   onClose(selectedDates) {
-    const deltaTime = startTime - currentTime; 
-    console.log(selectedDates[0]);
+    setInterval(() => {
+      const currentTime = Date.now;
+      const deltaTime = defaultDate - currentTime;
+      console.log(selectedDates[0]);
+      const timeComponents = convertMs(deltaTime);
+    }, 1000);
   },
 };
 
@@ -26,31 +31,18 @@ refs.startBtn.setAttribute('disabled', true);
 flatpickr(refs.text, options);
 let timerId = null;
 
-const timer = {
-  start() {
-    const startTime = Date.now();
-    
-    setInterval(() =>{
-const currentTime = Date.now();
-const deltaTime = startTime - currentTime; 
-const timeComponents = convertMs(deltaTime);
-    }, 1000);
-  },
-};
-timer.start();
+options.start();
 
 function convertMs() {
-  
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
- 
   const days = Math.floor(ms / day);
-    const hours = Math.floor((ms % day) / hour);
-    const minutes = Math.floor(((ms % day) % hour) / minute);
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
 }
@@ -59,12 +51,7 @@ console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
 console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
 console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
 
-  // if (deltaTime < 0) {
-  //     clearInterval(timer);
-  //     timer.innerHTML = "Час вийшов!";
-  //   }
-  // }, 1000);
-  
+
 // const timer = setInterval(function() {
 //   // Отримуємо поточну дату та час
 //   const now = new Date().getTime();
@@ -90,5 +77,84 @@ console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20
 // }, 1000);
 
 
+// import flatpickr from 'flatpickr';
+// import 'flatpickr/dist/flatpickr.min.css';
+// require('flatpickr/dist/themes/dark.css');
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
+// const refs = {
+//   btnTimerStart: document.querySelector('[data-start]'),
+//   timerFieldDays: document.querySelector('[data-days]'),
+//   timerFielHours: document.querySelector('[data-hours]'),
+//   timerFieldMinutes: document.querySelector('[data-minutes]'),
+//   timerFieldSeconds: document.querySelector('[data-seconds]'),
+// };
 
+// refs.btnTimerStart.disabled = true;
+// let timerId = null;
+
+// const options = {
+//   enableTime: true,
+//   time_24hr: true,
+//   defaultDate: new Date(),
+//   minuteIncrement: 1,
+
+//   onClose(selectedDates) {
+//     const currentDate = new Date();
+
+//     if (selectedDates[0] - currentDate > 0) {
+//       refs.btnTimerStart.disabled = false;
+//     } else {
+//       refs.btnTimerStart.disabled = true;
+//       Notify.failure('Please choose a date in the future', {
+//         timeout: 1500,
+//         width: '400px',
+//       });
+//     }
+//   },
+// };
+
+// function convertMs(ms) {
+//   const second = 1000;
+//   const minute = second * 60;
+//   const hour = minute * 60;
+//   const day = hour * 24;
+
+//   const days = Math.floor(ms / day);
+//   const hours = Math.floor((ms % day) / hour);
+//   const minutes = Math.floor(((ms % day) % hour) / minute);
+//   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+//   return { days, hours, minutes, seconds };
+// }
+
+// function addLeadingZero(value) {
+//   return String(value).padStart(2, 0);
+// }
+
+// function onTimerStart() {
+//   const selectedDate = fp.selectedDates[0];
+
+//   timerId = setInterval(() => {
+//     const startTime = new Date();
+//     const countdown = selectedDate - startTime;
+//     refs.btnTimerStart.disabled = true;
+
+//     if (countdown < 0) {
+//       clearInterval(timerId);
+//       return;
+//     }
+//     updateTimerFace(convertMs(countdown));
+//   }, 1_000);
+// }
+
+// function updateTimerFace({ days, hours, minutes, seconds }) {
+//   refs.timerFieldDays.textContent = addLeadingZero(days);
+//   refs.timerFielHours.textContent = addLeadingZero(hours);
+//   refs.timerFieldMinutes.textContent = addLeadingZero(minutes);
+//   refs.timerFieldSeconds.textContent = addLeadingZero(seconds);
+// }
+
+// const fp = flatpickr('#datetime-picker', options);
+
+// refs.btnTimerStart.addEventListener('click', onTimerStart);
