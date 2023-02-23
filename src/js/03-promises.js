7a917501a6cdb0002866a5117f88b2160ea61f0d
Inna Pokydko <inna.pokydko@gmail.com>
2023-02-23 19:45:18 +0200

    помилка 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,31 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
+  
+  const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    if (shouldResolve) {
+      resolve({ position, delay });
+      // Fulfill
+    } else {
     // Reject
-  }
+    reject({ position, delay });
+  }, delay);
+});
+
+promise
+  .then(({ position, delay }) => {
+    Notiflix.Notify.failure(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
 }
